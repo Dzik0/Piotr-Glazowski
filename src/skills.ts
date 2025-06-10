4c1c7ce48a4133ec94c1f5d67d2ce938516fc89f
Piotr <hunter976@gmail.com>
2025-06-11 00:44:33 +0200

    fixed logos

--- src/skills.ts
+++ src/skills.ts
@@ -8,41 +8,41 @@ export const skills: Skill[] = [
   {
     id: 1,
     name: 'React',
-    pic: '/react.png',
+    pic: './react.png',
   },
   {
     id: 2,
     name: 'Tailwind',
-    pic: '/tailwind.png',
+    pic: './tailwind.png',
   },
   {
     id: 3,
     name: 'JavaScript',
-    pic: '/js.png',
+    pic: './js.png',
   },
   {
     id: 4,
     name: 'TypeScript',
-    pic: '/ts.png',
+    pic: './ts.png',
   },
   {
     id: 5,
     name: 'Vite',
-    pic: '/vite.png',
+    pic: './vite.png',
   },
   {
     id: 6,
     name: 'Next.js',
-    pic: '/next.png',
+    pic: './next.png',
   },
   {
     id: 7,
     name: 'HTML',
-    pic: '/html.png',
+    pic: './html.png',
   },
   {
     id: 8,
     name: 'CSS',
-    pic: '/css.png',
+    pic: './css.png',
   },
 ];
